86eb2eb9ffd505e5b524d87f67e03cecbd27d3c9
Paulo Arthur <machineyordle@gmail.com>
2023-05-11 01:10:26 -0300

    Project created

---
+++ src/main/java/com/devSuperior/dslint/DslintApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.dslint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devSuperior/dslint/DslintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperior.dslint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
